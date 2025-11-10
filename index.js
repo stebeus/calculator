e9615e021885370cff8ef14da9dc8ea3fb5213c9
stebeus <stebeus@mailfence.com>
2025-11-10 16:23:33 -0300

    Clear index.js
    Follow the course's step-by-step construction to build each component properly and quickly.

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
 "use strict";
-
-const display = document.querySelector(".display");
-const containerNums = document.querySelector(".numbers");
-const btnNum = document.createElement("button");
-
-for (let i = 1; i <= 10; i++) {
-  btnNum.textContent = i - 1;
-  containerNums.appendChild(btnNum.cloneNode(i));
-}
