2bdc2c0f22ad6074daf5ed20183344196a014c4f
stebeus <stebeus@mailfence.com>
2025-11-10 16:37:55 -0300

    Create basic operation functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 "use strict";
+
+function add(...num) {
+  return num.reduce((total, current) => total + current);
+}
+
+function subtract(...num) {
+  return num.reduce((difference, current) => difference - current);
+}
+
+function multiply(...num) {
+  return num.reduce((product, current) => product * current);
+}
+
+function divide(...num) {
+  return num.reduce((quotient, current) => quotient / current);
+}
+
+console.log(
+  add(2, 4, 10),
+  subtract(16, 8),
+  subtract(16, 20),
+  multiply(8, 2),
+  divide(50, 5),
+  divide(50, 5, 2)
+);
