800e82277caf5ad3430e4310f4790a04a7f1ec04
stebeus <stebeus@mailfence.com>
2025-11-10 15:53:48 -0300

    Remove all query variables except display

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 "use strict";
 
 const display = document.querySelector(".display");
-const ctrlNumbers = document.querySelector(".numbers");
-const ctrlOperators = document.querySelector(".operators");
-
-const btnNumber = document.createElement("button");
-const btnOperator = document.createElement("button");
-
-btnNumber.classList.add("numbers__btn");
-btnOperator.classList.add("operators__btn");
