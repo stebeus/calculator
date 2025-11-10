83455660717d2f1c779982595fcb444a5709ba87
stebeus <stebeus@mailfence.com>
2025-11-10 16:08:16 -0300

    Create buttons in numbers with a loop

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 "use strict";
 
 const display = document.querySelector(".display");
+const containerNums = document.querySelector(".numbers");
+const btnNum = document.createElement("button");
+
+for (let i = 10; i > 0; i--) {
+  btnNum.textContent = i - 1;
+  containerNums.appendChild(btnNum.cloneNode(i));
+}
