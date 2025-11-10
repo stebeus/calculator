2a70a683284a150b522df3d0c7ac176462120d0d
stebeus <stebeus@mailfence.com>
2025-11-10 15:12:46 -0300

    Query select the index.html display, numbers and operators divs

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 "use strict";
 
+const display = document.querySelector(".display");
+const ctrlNumbers = document.querySelector(".numbers");
+const ctrlOperators = document.querySelector(".operators");
