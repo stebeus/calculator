d53a3b53334861aa68d213ece25ae2935657f376
stebeus <stebeus@mailfence.com>
2025-11-10 15:21:10 -0300

    Create numbers and operators button variables

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 const display = document.querySelector(".display");
 const ctrlNumbers = document.querySelector(".numbers");
 const ctrlOperators = document.querySelector(".operators");
+
+const btnNumber = document.createElement("button");
+const btnOperator = document.createElement("button");
+
+btnNumber.classList.add("numbers__btn");
+btnOperator.classList.add("operators__btn");
