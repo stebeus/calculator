e1895f626c9c6d917183e963e8993ab3626d2820
stebeus <stebeus@mailfence.com>
2025-11-10 16:14:42 -0300

    Use ascending increment in the button clone loop
    Use CSS flex to invert the button order.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const display = document.querySelector(".display");
 const containerNums = document.querySelector(".numbers");
 const btnNum = document.createElement("button");
 
-for (let i = 10; i > 0; i--) {
+for (let i = 1; i <= 10; i++) {
   btnNum.textContent = i - 1;
   containerNums.appendChild(btnNum.cloneNode(i));
 }
