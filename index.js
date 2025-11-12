afa65cb1344946e38fc1f215d7cbc8db3cd30fda
stebeus <stebeus@mailfence.com>
2025-11-12 19:16:10 -0300

    Separate with blank spaces each operation function

--- index.js
+++ index.js
@@ -37,12 +37,15 @@ function remove() {}
 function add(addendOne, addendTwo) {
   return addendOne + addendTwo;
 }
+
 function subtract(minuend, subtrahend) {
   return minuend - subtrahend;
 }
+
 function multiply(multiplicand, multiplier) {
   return multiplicand * multiplier;
 }
+
 function divide(dividend, divisor) {
   return dividend / divisor;
 }
