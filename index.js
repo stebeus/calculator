29689b9b86cdc686993e9b374e817844db1d0969
stebeus <stebeus@mailfence.com>
2025-11-12 17:19:55 -0300

    Query select main document elements

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const expression = document.querySelector("#expression");
+const result = document.querySelector("#result");
+const nums = document.querySelector(".nums");
+
+const btnClear = document.querySelector("[data-action='clear']");
+const btnRemove = document.querySelector("[data-action='remove']");
+const btnCalc = document.querySelector("[data-action='calc']");
+
+const inputNum = document.querySelectorAll("data-input='num'");
+const inputOperator = document.querySelectorAll("data-input='operator'");
