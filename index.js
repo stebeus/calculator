6aa960be0f7ed59cd0bdcbadc22cc830d383aa10
stebeus <stebeus@mailfence.com>
2025-11-12 17:40:00 -0300

    Fix syntax issues in query selectors and setAttributes

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 
 const expression = document.querySelector("#expression");
 const result = document.querySelector("#result");
-const nums = document.querySelector(".nums");
+const nums = document.querySelector(".numbers");
 
 const btnClear = document.querySelector("[data-action='clear']");
 const btnRemove = document.querySelector("[data-action='remove']");
 const btnCalc = document.querySelector("[data-action='calc']");
 
-const inputNum = document.querySelectorAll("data-input='num'");
-const inputOperator = document.querySelectorAll("data-input='operator'");
+const inputNum = document.querySelectorAll("[data-input='num']");
+const inputOperator = document.querySelectorAll("[data-input='operator']");
 
 const setAttributes = function (element, options) {
   Object.keys(options).forEach((attr) =>
-    element.setAttributes(attr, options[attr])
+    element.setAttribute(attr, options[attr])
   );
 };
 
