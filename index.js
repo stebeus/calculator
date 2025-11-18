6d7f3fba3cf88b86a23440bd3f5a7c73f80b3c13
stebeus <stebeus@mailfence.com>
2025-11-18 15:16:16 -0300

    Create basic math operation functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function add(addendOne, addendTwo) {
+  return addendOne + addendTwo;
+}
+
+function subtract(minuend, subtrahend) {
+  return minuend - subtrahend;
+}
+
+function multiply(multiplicand, multiplier) {
+  return multiplicand * multiplier;
+}
+
+function divide(dividend, divisor) {
+  return divisor === 0 ? "Undefined" : dividend / divisor;
+}
